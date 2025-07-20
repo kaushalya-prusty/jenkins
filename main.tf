5a26335f1f7144b366e0d0149c1a9bf21207c2f0
kausalya prusty <128567862+kaushalya-prusty@users.noreply.github.com>
2025-07-20 22:38:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "name" {
   ami           = "ami-0150ccaf51ab55a51"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0f9ac3729e6123609"
+  
  tags = {
     Name = "abhi"
   }
