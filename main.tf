3a1ae091c15b1438ab939de30ec47c3d127b8748
kausalya prusty <128567862+kaushalya-prusty@users.noreply.github.com>
2025-07-20 22:34:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,9 @@ resource "aws_instance" "name" {
   vpc_id        = "vpc-062a4301808bc530e"
   ami           = "ami-0150ccaf51ab55a51"
   instance_type = "t2.micro"
-  
   subnet_id = "subnet-0f9ac3729e6123609"
 
-
-  tags = {
+ tags = {
     Name = "abhi"
   }
 
